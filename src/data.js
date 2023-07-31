9f3bc25ae18d3cb99970ae414e806b3d96dac549
Andrea Garcia <anddysumptermx@gmail.com>
2023-07-31 10:40:11 -0600

    se agregaron fondos para cada tipo de pokemon

--- src/data.js
+++ src/data.js
@@ -1,9 +1,12 @@
-// estas funciones son de ejemplo
+// data.js
+import pokemonData from './data/pokemon.js';
 
-export const example = () => {
-  return 'example';
-};
+// Función para obtener todos los Pokémon
+export function getAllPokemon() {
+  return pokemonData.pokemon;
+}
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+// Función para filtrar los Pokémon por tipo o nombre
+export function filterData(pokemonList, value) {
+  return pokemonList.filter(pokemon => pokemon.name.toLowerCase().includes(value) || pokemon.type.includes(value));
+}
